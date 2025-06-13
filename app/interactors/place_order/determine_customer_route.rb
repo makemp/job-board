5c67a525d132b93af5960a7d67aeb9128c91fbdf
Maciej Kempin <maciej.kempin@gmail.com>
2025-06-13 23:10:47 +0100

    Free orders checkout

--- app/interactors/place_order/determine_customer_route.rb
+++ app/interactors/place_order/determine_customer_route.rb
@@ -8,10 +8,6 @@ class PlaceOrder
         context.redirect_path = first_confirmation_email_sent_path(free_order: context.order_placement.free?,
           needs_approval: !context.job_offer.approved?)
         Registrations::SendConfirmationEmailService.call!(context.info.email)
-      elsif context.order_placement.free?
-        context.redirect_path = job_offer_path(context.job_offer,
-          flash_notice: "Your job offer is now visible",
-          success: true)
       else
         session = Stripe::CheckoutSessionService.call(order_placement: context.order_placement)
         context.redirect_path = session.url
