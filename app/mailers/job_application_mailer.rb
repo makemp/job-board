4d2dc722296a4c5f368754082d4f8b7f2ec01f0d
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-08 13:33:57 +0100

    clamv and email fix

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -2,7 +2,7 @@ class JobApplicationMailer < ApplicationMailer
   def application_email(job_offer_application_id:)
     job_offer_application = JobOfferApplication.find(job_offer_application_id)
     @job_offer = job_offer_application.job_offer
-    attachments[job_offer_application.cv.original_filename] = job_offer_application.cv
+    attachments[job_offer_application.cv.blob.filename.to_s] = job_offer_application.cv.download
     @comments = job_offer_application.comments
     mail(
       to: @job_offer.application_destination,
