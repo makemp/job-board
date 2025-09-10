0f6cb801c459aa0ae09ba722faba641958e92ad6
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-10 10:58:05 +0100

    additional fixes

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -15,7 +15,7 @@ class ContactController < ApplicationController
     end
 
     if @contact.valid?
-      ContactMailer.contact_email(@contact).deliver_now
+      ContactMailer.contact_email(@contact).deliver_later
       redirect_to contact_path, notice: "Your message has been sent."
     else
       render :index, status: :unprocessable_content
