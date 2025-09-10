9d66615c21cee8852dc628640723ad2ea96532e2
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-10 23:59:21 +0100

    yes

--- app/jobs/job_alerts_job.rb
+++ app/jobs/job_alerts_job.rb
@@ -2,6 +2,7 @@ class JobAlertsJob < ApplicationJob
   queue_as :low_priority
 
   def perform(arguments = {})
+    arguments = arguments.deep_symbolize_keys
     job_offers_by_email = JobAlerts::FetchDataService.call(arguments[:frequency].to_sym)
     JobAlerts::SendEmailsService.call(job_offers_by_email)
   end
