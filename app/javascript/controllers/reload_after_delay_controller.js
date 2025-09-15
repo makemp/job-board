a2b8c44b2993aed55fa4928285035f0faa49715d
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-15 14:41:03 +0100

    further enhancements

--- app/javascript/controllers/reload_after_delay_controller.js
+++ app/javascript/controllers/reload_after_delay_controller.js
@@ -3,9 +3,20 @@ import { Turbo } from "@hotwired/turbo-rails"
 
 // Connects to data-controller="reload-after-delay"
 export default class extends Controller {
-  static values = { delay: Number, url: String }
+  static values = { delay: Number, url: String, maxReloads: { type: Number, default: 3 } }
 
   connect() {
+    const key = `reloadCount_${window.location.pathname}`;
+    const reloadCount = parseInt(sessionStorage.getItem(key) || "0");
+
+    if (reloadCount >= this.maxReloadsValue) {
+      sessionStorage.removeItem(key);
+      Turbo.visit("/");
+      return;
+    }
+
+    sessionStorage.setItem(key, reloadCount + 1);
+
     setTimeout(() => {
       if (this.urlValue) {
         Turbo.visit(this.urlValue)
@@ -15,4 +26,3 @@ export default class extends Controller {
     }, this.delayValue || 10000)
   }
 }
-
