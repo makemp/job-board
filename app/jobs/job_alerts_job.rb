66875298bd8ace0ef7f8226deb14fdb6a7a7158a
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-17 10:23:50 +0100

    betterjobs config

--- app/jobs/job_alerts_job.rb
+++ app/jobs/job_alerts_job.rb
@@ -2,8 +2,10 @@ class JobAlertsJob < ApplicationJob
   queue_as :low_priority
 
   def perform(arguments = {})
+    Rails.logger.info "JobAlertsJob started with arguments: #{arguments.inspect}"
     arguments = arguments.deep_symbolize_keys
     job_offers_by_email = JobAlerts::FetchDataService.call(arguments[:frequency].to_sym)
     JobAlerts::SendEmailsService.call(job_offers_by_email)
+    Rails.logger.info "JobAlertsJob completed"
   end
 end
