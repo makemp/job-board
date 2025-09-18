d351ca7b052b89863217152ec57ef485b25b1677
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-18 10:22:59 +0100

    the cron fix

--- app/jobs/job_alerts_job.rb
+++ app/jobs/job_alerts_job.rb
@@ -4,8 +4,25 @@ class JobAlertsJob < ApplicationJob
   def perform(arguments = {})
     Rails.logger.info "JobAlertsJob started with arguments: #{arguments.inspect}"
     arguments = arguments.deep_symbolize_keys
-    job_offers_by_email = JobAlerts::FetchDataService.call(arguments[:frequency].to_sym)
+    frequency = arguments[:frequency].to_sym
+
+    unless guard(frequency)
+      Rails.logger.info "JobAlertsJob skipped due to guard condition for frequency: #{frequency}"
+      return
+    end
+
+    job_offers_by_email = JobAlerts::FetchDataService.call(frequency)
     JobAlerts::SendEmailsService.call(job_offers_by_email)
     Rails.logger.info "JobAlertsJob completed"
+  end
+
+  private
+
+  def guard(frequency)
+    current_time = Time.current
+    return true if frequency == :monthly && current_time.day <= 7 && current_time.monday?
+    return true if frequency == :weekly && current_time.day > 7 && current_time.monday?
+    return true if frequency == :daily && !current_time.monday?
+    false
   end
 end
