366fc5844b12ff61c1205b0b4c19ee5f604e5400
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-29 12:27:34 +0100

    job offer directory

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -7,6 +7,7 @@ SitemapGenerator::Sitemap.create do
   add "/terms_and_conditions", priority: 0.7, changefreq: "monthly"
   add "/contact", priority: 0.2, changefreq: "monthly"
   add "/job_offer_forms/new", priority: 0.4, changefreq: "monthly"
+  add "/job_offers_directory", priority: 0.9, changefreq: "daily"
   BlogPost.where(published: true).each do |post|
     add Rails.application.routes.url_helpers.blog_post_path(post), lastmod: post.updated_at, priority: 0.9, changefreq: "monthly"
   end
