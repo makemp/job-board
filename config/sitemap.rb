45857bf8b44776e0fc3baa8ec20222e90eed1bd7
Maciej Kempin <maciej.kempin@gmail.com>
2025-09-29 16:07:59 +0100

    fix for sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -2,12 +2,11 @@
 SitemapGenerator::Sitemap.default_host = "https://drillcrew.work"
 
 SitemapGenerator::Sitemap.create do
-  add Rails.application.routes.url_helpers.root_path, priority: 1.0, changefreq: "daily"
   add "/privacy", priority: 0.7, changefreq: "monthly"
   add "/terms_and_conditions", priority: 0.7, changefreq: "monthly"
   add "/contact", priority: 0.2, changefreq: "monthly"
   add "/job_offer_forms/new", priority: 0.4, changefreq: "monthly"
-  add "/job_offers_directory", priority: 0.9, changefreq: "daily"
+  add "/job_offers_directory", priority: 1.0, changefreq: "daily"
   BlogPost.where(published: true).each do |post|
     add Rails.application.routes.url_helpers.blog_post_path(post), lastmod: post.updated_at, priority: 0.9, changefreq: "monthly"
   end
